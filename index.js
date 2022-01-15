7936ceb10033c34bda3c89ab5b17081375543850
Unzor <71789103+Unzor@users.noreply.github.com>
2022-01-14 20:40:48 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+var fs = require("fs");
+var chalk = require("chalk");
+var clojure = require("./clojure");
+var cli = require("cli2json").parse(process.argv.slice(2).join(" "));
+var f;
+if (cli.commands[0]) {
+var n = cli.commands[0];
+if (!n.endsWith(".clj")) {
+  if (!fs.existsSync(n)) {
+      n = n + ".clj";
+  }
+}
+f = fs.readFileSync(n).toString();  
+clojure.run(f);
+} else {
+var repl = require("repl");
+
+function myEval(cmd, context, filename, callback) {
+  var r = clojure.run(cmd).value || "undefined";
+  console.log(chalk.gray(r));
+  callback(null);
+}
+console.log("Welcome to Elephant REPL v1.0.0.\nPress ^C or ^D to exit, type .help for more info")
+repl.start({ prompt: '> ', eval: myEval });
+}
